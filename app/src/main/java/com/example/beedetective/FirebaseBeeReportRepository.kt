00d6d208e44748db6c105a397e0737751fe83791
ndyt <ntomlinson@gmail.com>
2022-05-07 17:56:26 -0500

    Committing before I make drastic changes to ViewModel

--- app/src/main/java/com/example/beedetective/FirebaseBeeReportRepository.kt
+++ app/src/main/java/com/example/beedetective/FirebaseBeeReportRepository.kt
@@ -1,6 +1,7 @@
 package com.example.beedetective
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.Query
@@ -14,6 +15,8 @@ class FirebaseBeeReportRepository(db: FirebaseFirestore): BeeReportRepository {
 
     // keep a reference to the listener so can stop listening when not needed.
     private var reportCollectionListener: ListenerRegistration? = null
+
+    val latestReports = MutableLiveData<List<BeeReport>>()
 
     override fun addReport(beeReport: BeeReport) {
         reportCollectionReference.add(beeReport)
@@ -35,7 +38,7 @@ class FirebaseBeeReportRepository(db: FirebaseFirestore): BeeReportRepository {
 
     override fun observeReports(function: (List<BeeReport>) -> Unit) {
         reportCollectionListener = reportCollectionReference
-            .orderBy("dateSpotted", Query.Direction.ASCENDING)
+            .orderBy("dateSpotted", Query.Direction.DESCENDING)
             .limit(10)
             .addSnapshotListener { snapshot, error ->
                 if (error != null) {
@@ -44,12 +47,12 @@ class FirebaseBeeReportRepository(db: FirebaseFirestore): BeeReportRepository {
                 if (snapshot != null) {
                     // FROM CLARA
                     // Simplest way - convert the snapshot to bee report objects.
-                    // val reports = snapshot.toObjects(Report::class.java)
+                     val reports = snapshot.toObjects(BeeReport::class.java)
 
                     // However, we want to store the report references so we'll need to loop and
                     // convert, and add the document references
 
-                    val reports = mutableListOf<BeeReport>()
+//                    val reports = mutableListOf<BeeReport>()
                     for (reportDocument in snapshot) {
                         val report = reportDocument.toObject(BeeReport::class.java)
                         report.documentReference = reportDocument.reference
@@ -57,7 +60,9 @@ class FirebaseBeeReportRepository(db: FirebaseFirestore): BeeReportRepository {
                         reports.add(report)
                     }
                     Log.d(TAG, "Reports from firebase: $reports")
-//                    notifyObserver(reports) --> code from Clara not working here
+//                    notifyObserver(reports)
+                    latestReports.postValue(reports)
+
                 }
             }
     }
