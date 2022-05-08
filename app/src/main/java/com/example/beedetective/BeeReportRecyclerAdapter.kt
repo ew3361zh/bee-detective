f176d7961de12fec40592f61a8550ceb5ccee1a6
ndyt <ntomlinson@gmail.com>
2022-05-07 23:30:28 -0500

    Able to get reports to load into feed fragment but photos not loading. Date and user notes loading

--- app/src/main/java/com/example/beedetective/BeeReportRecyclerAdapter.kt
+++ app/src/main/java/com/example/beedetective/BeeReportRecyclerAdapter.kt
@@ -17,15 +17,18 @@ class ReportRecyclerAdapter(var reports: List<BeeReport>):
     inner class ViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
         fun bind(report: BeeReport) {
             view.findViewById<TextView>(R.id.date_spotted).text = "${report.dateReported}"
-            Log.d(TAG, "date report is ${report.dateReported}")
+//            Log.d(TAG, "date report is ${report.dateReported}")
             view.findViewById<TextView>(R.id.usernotes).text = report.userNotes
-            Log.d(TAG, "usernotes are ${report.userNotes}")
+//            Log.d(TAG, "usernotes are ${report.userNotes}")
             // url link to photo in firebase storage
-            val photoPathReference = "gs://beedetective-af1e2.appspot.com/images/" + report.photoName
-
-            Picasso.get().load(photoPathReference).into(view.findViewById<ImageView>(R.id.bee_photo))
-//            view.findViewById<ImageView>(R.id.bee_photo)
-            // this is where we would find a piece of the tree fragment list item and set up a listener for it
+            val photoPathReference = "gs://beedetective-af1e2.appspot.com/images/" + report.photoName.toString()
+            Log.d(TAG, "photo path is $photoPathReference")
+            Picasso.get()
+                .load(photoPathReference)
+                .error(android.R.drawable.stat_notify_error) // displayed if issue with loading image
+                .fit()
+                .centerCrop()
+                .into(view.findViewById<ImageView>(R.id.bee_photo))
 
         }
     }
