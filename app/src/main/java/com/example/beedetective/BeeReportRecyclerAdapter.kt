e3a99219a085a2b91f2e28d1733f98edeff0e8ea
PearMyst <email>
2022-05-09 16:04:18 -0500

    Added ViewHolder ID to determine what views the recycler adapter should display depending on the bee report submitted.

--- app/src/main/java/com/example/beedetective/BeeReportRecyclerAdapter.kt
+++ app/src/main/java/com/example/beedetective/BeeReportRecyclerAdapter.kt
@@ -20,26 +20,39 @@ class ReportRecyclerAdapter(var reports: List<BeeReport>):
         fun bind(report: BeeReport) {
             view.findViewById<TextView>(R.id.date_spotted).text = "${report.dateReported}"
             // todo user notes can be blank, should we add default value for note?
-            view.findViewById<TextView>(R.id.usernotes).text = report.userNotes
+
+            if (!report.userNotes.isNullOrBlank()) {
+                view.findViewById<TextView>(R.id.usernotes).text = report.userNotes
+            }
+
             // url link to photo in firebase storage
-            val photoHttpPathReference = "https://firebasestorage.googleapis.com/v0/b/beedetective-af1e2.appspot.com/o/images%2F" +
-                    report.photoName.toString() + "?alt=media&token=c0d00bf1-f1f2-467a-82cf-2ce522068cdb"
-            Log.d(TAG, "photo path is $photoHttpPathReference")
-            // todo what if user doesn't take photo or photo can't load - maybe have default bee photo inserted
-            // todo ideally would only get reports with photos but maybe important to have location data
-            //  which we could use to launch a map intent?
-            Picasso.get()
-                .load(photoHttpPathReference)
-                .error(android.R.drawable.stat_notify_error) // displayed if issue with loading image
-                .fit()
-                .centerCrop()
-                .into(view.findViewById<ImageView>(R.id.bee_photo))
+            if (report.photoName != null) {
+                val photoHttpPathReference =
+                    "https://firebasestorage.googleapis.com/v0/b/beedetective-af1e2.appspot.com/o/images%2F" +
+                            report.photoName.toString() + "?alt=media&token=c0d00bf1-f1f2-467a-82cf-2ce522068cdb"
+                Log.d(TAG, "photo path is $photoHttpPathReference")
+                // todo what if user doesn't take photo or photo can't load - maybe have default bee photo inserted
+                // todo ideally would only get reports with photos but maybe important to have location data
+                //  which we could use to launch a map intent?
+                Picasso.get()
+                    .load(photoHttpPathReference)
+                    .error(android.R.drawable.stat_notify_error) // displayed if issue with loading image
+                    .fit()
+                    .centerCrop()
+                    .into(view.findViewById<ImageView>(R.id.bee_photo))
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.fragment_bee_report_feed_item, parent, false)
+        val view = when (viewType) {
+            2 -> LayoutInflater.from(parent.context)
+                .inflate(R.layout.fragment_bee_report_feed_item, parent, false)
+            1 -> LayoutInflater.from(parent.context)
+                .inflate(R.layout.fragment_bee_report_no_notes_feed_item, parent, false)
+            else -> LayoutInflater.from(parent.context) //TODO adjust no photo layout. maybe include a icon or logo.
+                .inflate(R.layout.fargment_bee_report_no_photo_feed_item, parent, false)
+        }
         return ViewHolder(view)
     }
 
@@ -51,6 +64,17 @@ class ReportRecyclerAdapter(var reports: List<BeeReport>):
 
     override fun getItemCount(): Int {
         return reports.size
+    }
+
+    // Determines what view we should display in the recycler.
+    override fun getItemViewType(position: Int): Int {
+        val report = reports[position]
+
+        return when {
+            report.photoName == null -> 0
+            report.userNotes.isNullOrBlank() -> 1
+            else -> 2
+        }
     }
 
 
